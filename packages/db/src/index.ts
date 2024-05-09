ede2fe1b0e9880c1fefb29e795fe25d525c71f83
Cristi <raducristiandumitrana@gmail.com>
2024-05-09 12:32:05 +0300

    move settings

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,14 +1,32 @@
-import { Client } from "@planetscale/database";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { createClient, Duration, IsolationLevel } from "edgedb";
 
-import { connectionStr } from "./config";
-import * as auth from "./schema/auth";
-import * as post from "./schema/post";
+const baseClient = createClient({
+  host: "localhost",
+  port: 5656,
+  user: "admin",
+  database: "edgedb",
+  password: "admin",
+});
 
-export * from "drizzle-orm/sql";
-export { alias } from "drizzle-orm/mysql-core";
-
-export const schema = { ...auth, ...post };
-
-const psClient = new Client({ url: connectionStr.href });
-export const db = drizzle(psClient, { schema });
+export const db = baseClient
+  .withConfig({
+    // 10 seconds
+    session_idle_transaction_timeout: Duration.from({ seconds: 10 }),
+    // 0 seconds === no timeout
+    query_execution_timeout: Duration.from({ seconds: 0 }),
+    allow_bare_ddl: "NeverAllow",
+    allow_user_specified_id: false,
+    apply_access_policies: true,
+  })
+  .withRetryOptions({
+    attempts: 3,
+    backoff: (attemptNo: number) => {
+      // exponential backoff
+      return 2 ** attemptNo * 100 + Math.random() * 100;
+    },
+  })
+  .withTransactionOptions({
+    isolation: IsolationLevel.Serializable, // only supported value
+    deferrable: false,
+    readonly: false,
+  });
