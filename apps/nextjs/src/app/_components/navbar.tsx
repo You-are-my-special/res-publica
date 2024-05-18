8561b4ab1126924b85524b3df7c56b3bee5e39c1
andrewdoro <elfandreis@gmail.com>
2024-05-18 12:27:33 +0300

    feat: add repo navbar nav

--- apps/nextjs/src/app/_components/navbar.tsx
+++ apps/nextjs/src/app/_components/navbar.tsx
@@ -1,15 +1,36 @@
 import React from "react";
-import { Github } from "lucide-react";
+import Link from "next/link";
+import {
+  BookAIcon,
+  BookMarked,
+  CircleAlert,
+  CircleDot,
+  Github,
+} from "lucide-react";
 
 import { Button } from "@acme/ui/button";
 import { ThemeToggle } from "@acme/ui/theme";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between py-4">
-      <Button size="icon" variant="outline">
-        <Github />
-      </Button>
+    <div className="flex items-center justify-between py-4">
+      <div className="flex items-center gap-2">
+        <Button size="icon" variant="outline">
+          <Github />
+        </Button>
+        <Link href="/">
+          <Button variant="outline" className="gap-1">
+            <CircleDot className="h-4" />
+            Issues
+          </Button>
+        </Link>
+        <Link href="/repo">
+          <Button variant="outline" className="gap-1">
+            <BookMarked className="h-4" />
+            Repos
+          </Button>
+        </Link>
+      </div>
       <ThemeToggle />
     </div>
   );
